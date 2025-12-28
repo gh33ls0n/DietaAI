68b062ea88cf350a53144f2f9a675d8abf7e2b6c
gh33ls0n <55130851+gh33ls0n@users.noreply.github.com>
2025-12-28 16:15:45 +0100

    kolejny duży update bazy posiłków

--- services/mealDatabase.ts
+++ services/mealDatabase.ts
@@ -37,10 +37,9 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Chleb żytni", amount: "2 kromki" },
       { item: "Awokado", amount: "1/2 szt" },
       { item: "Jajko", amount: "1 szt" },
-      { item: "Oliwa z oliwek", amount: "1 łyżeczka" },
-      { item: "Płatki chili", amount: "szczypta" }
+      { item: "Oliwa z oliwek", amount: "1 łyżeczka" }
     ],
-    recipe: "Awokado rozgnieć na chlebie. Na patelni przygotuj jajko sadzone na oliwie i połóż na wierzch."
+    recipe: "Awokado rozgnieć na chlebie. Na patelni przygotuj jajko sadzone i połóż na wierzch."
   },
   {
     name: "Kanapki z mozzarellą i pomidorem (Caprese)",
@@ -50,8 +49,7 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Bułka grahamka", amount: "1 szt" },
       { item: "Mozzarella light", amount: "1/2 kulki" },
       { item: "Pomidor", amount: "1/2 szt" },
-      { item: "Pesto bazyliowe", amount: "1 łyżeczka" },
-      { item: "Świeża bazylia", amount: "kilka liści" }
+      { item: "Pesto bazyliowe", amount: "1 łyżeczka" }
     ],
     recipe: "Bułkę przekrój, posmaruj pesto. Ułóż plastry sera i pomidora, udekoruj bazylią."
   },
@@ -63,10 +61,9 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Chleb pełnoziarnisty", amount: "2 kromki" },
       { item: "Makrela wędzona", amount: "60g" },
       { item: "Twaróg chudy", amount: "30g" },
-      { item: "Cebula biała", amount: "1/4 szt" },
-      { item: "Koncentrat pomidorowy", amount: "1 łyżeczka" }
+      { item: "Cebula biała", amount: "1/4 szt" }
     ],
-    recipe: "Makrelę wymieszaj z twarogiem, posiekaną cebulą i koncentratem. Nałóż na pieczywo."
+    recipe: "Makrelę wymieszaj z twarogiem i cebulą. Nałóż na pieczywo."
   },
   {
     name: "Kanapki z serem camembert i żurawiną",
@@ -75,11 +72,10 @@ export const MEAL_DATABASE: Meal[] = [
     ingredients: [
       { item: "Chleb żytni", amount: "2 kromki" },
       { item: "Ser Camembert", amount: "40g" },
-      { item: "Żurawina do mięs/serów", amount: "2 łyżeczki" },
-      { item: "Orzechy włoskie", amount: "2 szt" },
-      { item: "Rukola", amount: "garść" }
+      { item: "Żurawina do mięs", amount: "2 łyżeczki" },
+      { item: "Orzechy włoskie", amount: "2 szt" }
     ],
-    recipe: "Na chlebie ułóż rukolę, plastry sera, dodaj żurawinę i pokruszone orzechy."
+    recipe: "Na chlebie ułóż plastry sera, dodaj żurawinę i pokruszone orzechy."
   },
   {
     name: "Kanapki z hummusem i suszonymi pomidorami",
@@ -88,13 +84,12 @@ export const MEAL_DATABASE: Meal[] = [
     ingredients: [
       { item: "Chleb wieloziarnisty", amount: "2 kromki" },
       { item: "Hummus", amount: "3 łyżki" },
-      { item: "Suszone pomidory", amount: "3 szt" },
-      { item: "Kiełki", amount: "garść" }
+      { item: "Suszone pomidory", amount: "3 szt" }
     ],
-    recipe: "Chleb posmaruj hummusem. Połóż suszone pomidory i posyp kiełkami."
+    recipe: "Chleb posmaruj hummusem, połóż pomidory i ulubione kiełki."
   },
   {
-    name: "Kanapki z domową pastą jajeczną",
+    name: "Kanapki z pastą jajeczną i szczypiorkiem",
     calories: 390, protein: 16, fats: 18, carbs: 42,
     type: "breakfast",
     ingredients: [
@@ -103,132 +98,192 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Majonez lekki", amount: "1 łyżeczka" },
       { item: "Szczypiorek", amount: "1 łyżka" }
     ],
-    recipe: "Jajko rozgnieć, wymieszaj z majonezem i szczypiorkiem. Nałóż na pieczywo."
+    recipe: "Jajko rozgnieć, wymieszaj z majonezem i szczypiorkiem. Nałóż na chleb."
+  },
+  {
+    name: "Kanapki z pieczonym indykiem i żurawiną",
+    calories: 365, protein: 24, fats: 9, carbs: 46,
+    type: "breakfast",
+    ingredients: [
+      { item: "Bułka grahamka", amount: "1 szt" },
+      { item: "Pierś z indyka pieczona", amount: "60g" },
+      { item: "Dżem z żurawiny", amount: "1 łyżeczka" }
+    ],
+    recipe: "Bułkę przekrój, ułóż sałatę, indyka i posmaruj żurawiną."
+  },
+  {
+    name: "Twarożek ze szczypiorkiem na żytnim",
+    calories: 340, protein: 22, fats: 8, carbs: 45,
+    type: "breakfast",
+    ingredients: [
+      { item: "Chleb żytni", amount: "2 kromki" },
+      { item: "Twaróg chudy", amount: "100g" },
+      { item: "Jogurt naturalny", amount: "2 łyżki" },
+      { item: "Rzodkiewka i szczypiorek", amount: "do smaku" }
+    ],
+    recipe: "Twaróg wymieszaj z jogurtem i warzywami. Nałóż na pieczywo."
   },
 
   // --- OBIADY (lunch) ---
   {
-    name: "Spaghetti Bolognese z chudym indykiem",
+    name: "Spaghetti Bolognese z indykiem",
     calories: 540, protein: 32, fats: 14, carbs: 65,
     type: "lunch",
     ingredients: [
       { item: "Makaron spaghetti pełnoziarnisty", amount: "70g" },
       { item: "Mięso mielone z indyka", amount: "120g" },
-      { item: "Passata pomidorowa", amount: "150ml" },
-      { item: "Cebula i czosnek", amount: "1 szt" },
-      { item: "Parmezan", amount: "1 łyżka" }
+      { item: "Passata pomidorowa", amount: "150ml" }
     ],
-    recipe: "Mięso podsmaż z cebulą i czosnkiem. Dodaj passatę i duś 10 min. Podawaj z ugotowanym makaronem i parmezanem."
+    recipe: "Mięso podsmaż, dodaj passatę. Duś 10 min. Wymieszaj z ugotowanym makaronem."
   },
   {
-    name: "Leczo warzywne z chudą kiełbasą i kaszą",
+    name: "Spaghetti Carbonara w wersji fit",
+    calories: 550, protein: 28, fats: 18, carbs: 62,
+    type: "lunch",
+    ingredients: [
+      { item: "Makaron spaghetti", amount: "70g" },
+      { item: "Szynka dojrzewająca", amount: "30g" },
+      { item: "Jajko", amount: "1 szt" },
+      { item: "Parmezan", amount: "2 łyżki" }
+    ],
+    recipe: "Gorący makaron wymieszaj z roztrzepanym jajkiem i serem poza ogniem."
+  },
+  {
+    name: "Leczo warzywne z kiełbasą drobiową",
     calories: 490, protein: 25, fats: 18, carbs: 55,
     type: "lunch",
     ingredients: [
       { item: "Cukinia", amount: "1/2 szt" },
-      { item: "Papryka kolorowa", amount: "1 szt" },
-      { item: "Kiełbasa drobiowa chuda", amount: "80g" },
-      { item: "Kasza pęczak", amount: "50g" },
-      { item: "Przecier pomidorowy", amount: "2 łyżki" }
+      { item: "Papryka", amount: "1 szt" },
+      { item: "Kiełbasa drobiowa", amount: "80g" },
+      { item: "Kasza pęczak", amount: "50g" }
     ],
-    recipe: "Warzywa i kiełbasę pokrój, duś w garnku z przecierem i przyprawami. Podawaj z ugotowaną kaszą."
+    recipe: "Warzywa i kiełbasę podduś w garnku. Podawaj z ugotowaną kaszą."
   },
   {
-    name: "Gulasz wołowy z kaszą gryczaną i ogórkiem",
+    name: "Gulasz z szynki z kaszą gryczaną",
     calories: 560, protein: 38, fats: 16, carbs: 60,
     type: "lunch",
     ingredients: [
-      { item: "Chuda wołowina", amount: "130g" },
+      { item: "Szynka wieprzowa", amount: "130g" },
       { item: "Kasza gryczana", amount: "50g" },
-      { item: "Ogórek kiszony", amount: "2 szt" },
-      { item: "Marchewka", amount: "1 szt" }
+      { item: "Ogórek kiszony", amount: "2 szt" }
     ],
-    recipe: "Wołowinę duś z marchewką do miękkości. Podawaj z ugotowaną kaszą i ogórkiem kiszonym."
+    recipe: "Mięso i warzywa duś do miękkości. Podawaj z kaszą i ogórkiem."
   },
   {
-    name: "Kurczak po chińsku z warzywami i ryżem",
-    calories: 520, protein: 34, fats: 10, carbs: 70,
+    name: "Kurczak po chińsku z warzywami (Stir-fry)",
+    calories: 530, protein: 35, fats: 10, carbs: 72,
     type: "lunch",
     ingredients: [
-      { item: "Pierś z kurczaka", amount: "120g" },
-      { item: "Mieszanka chińska warzyw", amount: "200g" },
       { item: "Ryż basmati", amount: "60g" },
-      { item: "Sos sojowy", amount: "2 łyżki" },
-      { item: "Imbir świeży", amount: "kawałek" }
+      { item: "Kurczak", amount: "120g" },
+      { item: "Mieszanka warzyw chińska", amount: "200g" }
     ],
-    recipe: "Kurczaka i warzywa smaż krótko na dużym ogniu z imbirem i sosem sojowym. Podawaj z ryżem."
+    recipe: "Smaż kurczaka z warzywami na dużym ogniu z sosem sojowym. Podawaj z ryżem."
   },
   {
-    name: "Wietnamska sałatka Bun Cha z kurczakiem",
-    calories: 480, protein: 30, fats: 12, carbs: 62,
-    type: "lunch",
-    ingredients: [
-      { item: "Makaron ryżowy", amount: "50g" },
-      { item: "Grillowana pierś z kurczaka", amount: "120g" },
-      { item: "Marchewka i ogórek", amount: "1 szt" },
-      { item: "Orzeszki ziemne", amount: "1 łyżeczka" },
-      { item: "Sos rybny i limonka", amount: "do smaku" }
-    ],
-    recipe: "Makaron namocz, kurczaka grilluj. Wymieszaj z warzywami, orzeszkami i sosem na bazie limonki."
-  },
-  {
-    name: "Fit Burger Wołowy z frytkami z batatów",
-    calories: 580, protein: 38, fats: 20, carbs: 62,
+    name: "Fit Burger Wołowy z batatami",
+    calories: 585, protein: 39, fats: 21, carbs: 64,
     type: "lunch",
     ingredients: [
       { item: "Bułka pełnoziarnista", amount: "1 szt" },
       { item: "Wołowina mielona chuda", amount: "150g" },
-      { item: "Bataty", amount: "150g" },
-      { item: "Sałata i pomidor", amount: "do smaku" }
+      { item: "Batat", amount: "150g" }
     ],
-    recipe: "Burger grilluj, bataty upiecz jako frytki. Złóż z warzywami w bułce."
+    recipe: "Mięso ugrilluj, batata upiecz w słupkach. Złóż burgera z warzywami."
   },
   {
-    name: "Fit Pizza z szynką i rukolą",
-    calories: 540, protein: 32, fats: 16, carbs: 68,
+    name: "Pizza fit na cienkim cieście z szynką",
+    calories: 550, protein: 33, fats: 17, carbs: 69,
     type: "lunch",
     ingredients: [
       { item: "Spód pełnoziarnisty", amount: "1/2 szt" },
       { item: "Mozzarella light", amount: "50g" },
-      { item: "Szynka drobiowa", amount: "3 plastry" },
-      { item: "Rukola", amount: "garść" }
+      { item: "Szynka drobiowa", amount: "3 plastry" }
     ],
-    recipe: "Spód posmaruj pomidorami, dodaj ser i szynkę. Piecz 10 min. Posyp rukolą."
+    recipe: "Na spód nałóż passatę, ser i szynkę. Piecz 10 min w 220st."
   },
   {
-    name: "Kotlety mielone z indyka, ziemniaki i mizeria",
-    calories: 510, protein: 35, fats: 15, carbs: 58,
+    name: "Kotlety mielone z indyka z purée ziemniaczanym",
+    calories: 515, protein: 34, fats: 15, carbs: 56,
     type: "lunch",
     ingredients: [
+      { item: "Mięso mielone z indyka", amount: "120g" },
       { item: "Ziemniaki", amount: "200g" },
-      { item: "Mielone z indyka (pieczone)", amount: "120g" },
-      { item: "Ogórek świeży", amount: "1 szt" },
-      { item: "Jogurt naturalny", amount: "2 łyżki" }
+      { item: "Buraczki tarte", amount: "100g" }
     ],
-    recipe: "Kotlety upiecz w piekarniku. Ziemniaki ugotuj. Ogórka pokrój i wymieszaj z jogurtem."
+    recipe: "Kotlety upiecz w piekarniku. Ziemniaki ugnieć na purée. Podawaj z buraczkami."
   },
   {
-    name: "Schab pieczony w ziołach z ziemniakami",
-    calories: 530, protein: 36, fats: 14, carbs: 60,
+    name: "Schabowy w otrębach, ziemniaki i mizeria",
+    calories: 560, protein: 38, fats: 18, carbs: 59,
     type: "lunch",
     ingredients: [
-      { item: "Schab pieczony", amount: "130g" },
+      { item: "Schab bez kości", amount: "130g" },
       { item: "Ziemniaki", amount: "200g" },
-      { item: "Buraczki ugotowane", amount: "100g" }
+      { item: "Ogórek świeży", amount: "1 szt" }
     ],
-    recipe: "Schab upiecz z majerankiem. Podawaj z ugotowanymi ziemniakami i buraczkami."
+    recipe: "Schab panieruj w jajku i otrębach, upiecz. Podawaj z ziemniakami i mizerią."
   },
+
+  // --- OBIADY (lunch) - RYBY ---
   {
-    name: "Stir-fry z kurczakiem i orzechami nerkowca",
-    calories: 550, protein: 35, fats: 18, carbs: 58,
+    name: "Pieczony łosoś z koperkiem i ziemniakami",
+    calories: 520, protein: 34, fats: 18, carbs: 54,
     type: "lunch",
     ingredients: [
-      { item: "Ryż jaśminowy", amount: "50g" },
-      { item: "Kurczak", amount: "120g" },
-      { item: "Brokuł", amount: "1/2 szt" },
-      { item: "Orzechy nerkowca", amount: "15g" }
+      { item: "Łosoś świeży filet", amount: "150g" },
+      { item: "Ziemniaki", amount: "200g" },
+      { item: "Koper", amount: "1/2 pęczka" },
+      { item: "Cytryna", amount: "2 plastry" }
     ],
-    recipe: "Kurczaka i brokuły smaż szybko na patelni. Na koniec dodaj orzechy i sos sojowy. Podawaj z ryżem."
+    recipe: "Łososia upiecz w 180st (15 min). Podawaj z ugotowanymi ziemniakami z koperkiem."
+  },
+  {
+    name: "Dorsz świeży z pary, ryż i brokuły",
+    calories: 480, protein: 30, fats: 10, carbs: 65,
+    type: "lunch",
+    ingredients: [
+      { item: "Dorsz filet", amount: "150g" },
+      { item: "Ryż jaśminowy", amount: "60g" },
+      { item: "Brokuły", amount: "150g" }
+    ],
+    recipe: "Dorsza i brokuły ugotuj na parze. Podawaj z ryżem i cytryną."
+  },
+  {
+    name: "Pstrąg pieczony w folii z czosnkiem",
+    calories: 495, protein: 35, fats: 14, carbs: 52,
+    type: "lunch",
+    ingredients: [
+      { item: "Pstrąg patroszony", amount: "1 szt" },
+      { item: "Ziemniaki pieczone", amount: "200g" },
+      { item: "Czosnek i zioła", amount: "do smaku" }
+    ],
+    recipe: "Rybę nafaszeruj czosnkiem i upiecz w folii (25 min). Podawaj z ziemniakami."
+  },
+  {
+    name: "Miruna w panierce z otrębów z kaszą",
+    calories: 510, protein: 32, fats: 12, carbs: 68,
+    type: "lunch",
+    ingredients: [
+      { item: "Miruna filet", amount: "140g" },
+      { item: "Otręby owsiane", amount: "2 łyżki" },
+      { item: "Kasza gryczana", amount: "50g" }
+    ],
+    recipe: "Rybę panieruj w jajku i otrębach, upiecz bez tłuszczu. Podawaj z kaszą."
+  },
+  {
+    name: "Łosoś Teriyaki z fasolką szparagową",
+    calories: 550, protein: 36, fats: 20, carbs: 58,
+    type: "lunch",
+    ingredients: [
+      { item: "Łosoś filet", amount: "150g" },
+      { item: "Sos Teriyaki", amount: "2 łyżki" },
+      { item: "Ryż biały", amount: "50g" },
+      { item: "Fasolka szparagowa", amount: "150g" }
+    ],
+    recipe: "Łososia upiecz w sosie. Podawaj z ryżem i gotowaną fasolką."
   },
 
   // --- PODWIECZORKI I PRZEKĄSKI (snack2) ---
@@ -241,7 +296,7 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Borówki", amount: "garść" },
       { item: "Migdały", amount: "10 szt" }
     ],
-    recipe: "Owoce i posiekane migdały dodaj do jogurtu."
+    recipe: "Składniki wymieszaj w miseczce."
   },
   {
     name: "Sałatka owocowa z orzechami",
@@ -252,10 +307,10 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Pomarańcza", amount: "1/2 szt" },
       { item: "Orzechy włoskie", amount: "3 szt" }
     ],
-    recipe: "Owoce pokrój w kostkę, wymieszaj i posyp orzechami."
+    recipe: "Owoce pokrój w kostkę i posyp orzechami."
   },
   {
-    name: "Serek wiejski z rzodkiewką",
+    name: "Serek wiejski z rzodkiewką i szczypiorkiem",
     calories: 190, protein: 18, fats: 6, carbs: 12,
     type: "snack2",
     ingredients: [
@@ -305,7 +360,7 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Napój migdałowy", amount: "150ml" },
       { item: "Maliny", amount: "garść" }
     ],
-    recipe: "Chia zalej napojem, odstaw na noc. Podawaj z malinami."
+    recipe: "Chia namocz w napoju na noc. Podawaj z malinami."
   },
   {
     name: "Kefir z otrębami i cynamonem",
@@ -319,7 +374,7 @@ export const MEAL_DATABASE: Meal[] = [
     recipe: "Wymieszaj otręby z kefirem i posyp cynamonem."
   },
   {
-    name: "Wafle ryżowe z twarożkiem",
+    name: "Wafle ryżowe z twarożkiem i dżemem",
     calories: 195, protein: 12, fats: 3, carbs: 30,
     type: "snack2",
     ingredients: [
@@ -330,13 +385,13 @@ export const MEAL_DATABASE: Meal[] = [
     recipe: "Wafle posmaruj twarożkiem i dodaj dżem."
   },
   {
-    name: "Garść orzechów nerkowca",
+    name: "Orzechy nerkowca i suszone morele",
     calories: 245, protein: 5, fats: 16, carbs: 22,
     type: "snack2",
     ingredients: [
       { item: "Orzechy nerkowca", amount: "20g" },
       { item: "Morele suszone", amount: "3 szt" }
     ],
-    recipe: "Szybka przekąska w trasie."
+    recipe: "Szybka przekąska do ręki."
   }
 ];
