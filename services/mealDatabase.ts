fb9b492b2d5bbbc0e8de9155bf9ad6dec50d1d1b
gh33ls0n <55130851+gh33ls0n@users.noreply.github.com>
2025-12-28 19:15:46 +0100

    kolejny udate obiadów z aniagotuje i kwestia smaku

--- services/mealDatabase.ts
+++ services/mealDatabase.ts
@@ -2,7 +2,7 @@
 import { Meal } from "../types";
 
 export const MEAL_DATABASE: Meal[] = [
-  // --- KANAPKI I ŚNIADANIA (breakfast, snack1, dinner) ---
+  // --- ŚNIADANIA I KOLACJE (breakfast, snack1, dinner) ---
   {
     name: "Kanapki z szynką, serem i ogórkiem kiszonym",
     calories: 360, protein: 18, fats: 12, carbs: 45,
@@ -27,7 +27,7 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Oliwa z oliwek", amount: "1 łyżeczka" },
       { item: "Czosnek", amount: "1 ząbek" }
     ],
-    recipe: "Na patelni zeszklij cebulę i czosnek. Dodaj pokrojone pomidory i smaż aż zmiękną. Wbij jajka, przykryj patelnię i czekaj, aż białka się zetną, a żółtka pozostaną płynne."
+    recipe: "Na patelni zeszklij cebulę i czosnek. Dodaj pokrojone pomidory i smaż aż zmiękną. Wbij jajka, przykryj patelnię i czekaj aż się zetną."
   },
   {
     name: "Fit Placuszki z jabłkiem i cynamonem",
@@ -37,10 +37,20 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Jabłko", amount: "150g" },
       { item: "Jajko (rozmiar L)", amount: "1 szt" },
       { item: "Skyr naturalny", amount: "50g" },
-      { item: "Mąka pełnoziarnista", amount: "3 łyżki" },
-      { item: "Cynamon", amount: "szczypta" }
+      { item: "Mąka pełnoziarnista", amount: "3 łyżki" }
     ],
-    recipe: "Jabłko zetrzyj na tarce o grubych oczkach. Wymieszaj z jajkiem, skyrem i mąką. Smaż małe placuszki na suchej patelni teflonowej do zrumienienia."
+    recipe: "Jabłko zetrzyj, wymieszaj z jajkiem, skyrem i mąką. Smaż na suchej patelni."
+  },
+  {
+    name: "Kanapki z wędzonym łososiem i twarożkiem",
+    calories: 380, protein: 22, fats: 14, carbs: 40,
+    type: "breakfast",
+    ingredients: [
+      { item: "Chleb graham", amount: "2 kromki" },
+      { item: "Twarożek kanapkowy lekki", amount: "2 łyżki" },
+      { item: "Łosoś wędzony", amount: "50g" }
+    ],
+    recipe: "Chleb posmaruj twarożkiem, połóż łososia i koper."
   },
   {
     name: "Pasta jajeczna ze szczypiorkiem",
@@ -49,10 +59,20 @@ export const MEAL_DATABASE: Meal[] = [
     ingredients: [
       { item: "Jajko (rozmiar L)", amount: "2 szt" },
       { item: "Jogurt naturalny", amount: "1 łyżka" },
-      { item: "Szczypiorek", amount: "2 łyżki" },
-      { item: "Musztarda", amount: "1 łyżeczka" }
+      { item: "Szczypiorek", amount: "2 łyżki" }
     ],
-    recipe: "Jajka ugotuj na twardo, posiekaj drobno. Wymieszaj z jogurtem, musztardą i szczypiorkiem. Podawaj z pieczywem."
+    recipe: "Jajka ugotuj, posiekaj i wymieszaj z jogurtem oraz szczypiorkiem."
+  },
+  {
+    name: "Grzanki z awokado i jajkiem sadzonym",
+    calories: 420, protein: 16, fats: 22, carbs: 38,
+    type: "breakfast",
+    ingredients: [
+      { item: "Chleb żytni", amount: "2 kromki" },
+      { item: "Awokado", amount: "1/2 szt" },
+      { item: "Jajko", amount: "1 szt" }
+    ],
+    recipe: "Awokado rozgnieć na chlebie, połóż usmażone jajko."
   },
   {
     name: "Fit Kluski leniwe z jogurtem",
@@ -61,26 +81,23 @@ export const MEAL_DATABASE: Meal[] = [
     ingredients: [
       { item: "Twaróg chudy", amount: "200g" },
       { item: "Jajko (rozmiar L)", amount: "1 szt" },
-      { item: "Mąka pełnoziarnista", amount: "4 łyżki" },
-      { item: "Jogurt naturalny", amount: "2 łyżki" }
+      { item: "Mąka pełnoziarnista", amount: "4 łyżki" }
     ],
-    recipe: "Twaróg rozgnieć z jajkiem i mąką. Uformuj kluski, gotuj 2 min od wypłynięcia."
+    recipe: "Zagnieć twaróg z jajkiem i mąką. Gotuj 2 min od wypłynięcia. Podawaj z jogurtem."
   },
 
   // --- OBIADY (lunch) ---
   {
-    name: "Burger Wołowy Fit z pieczonymi batatami",
+    name: "Burger Wołowy Fit z batatami",
     calories: 580, protein: 38, fats: 18, carbs: 65,
     type: "lunch",
     ingredients: [
       { item: "Wołowina mielona chuda", amount: "150g" },
       { item: "Bułka do burgera pełnoziarnista", amount: "1 szt" },
       { item: "Bataty", amount: "150g" },
-      { item: "Sałata lodowa", amount: "2 liście" },
-      { item: "Pomidor", amount: "2 plastry" },
-      { item: "Ketchup bez cukru", amount: "1 łyżka" }
+      { item: "Sałata lodowa", amount: "2 liście" }
     ],
-    recipe: "Mięso uformuj w burgera i upiecz lub grilluj bez tłuszczu. Bataty pokrój w frytki i piecz 20 min w 200 st. Złóż burgera z warzywami i sosem."
+    recipe: "Mięso grilluj bez tłuszczu. Bataty upiecz w 200st (25 min). Złóż burgera z warzywami."
   },
   {
     name: "Pizza Fit na cienkim cieście",
@@ -88,12 +105,11 @@ export const MEAL_DATABASE: Meal[] = [
     type: "lunch",
     ingredients: [
       { item: "Mąka pełnoziarnista", amount: "80g" },
-      { item: "Passata pomidorowa", amount: "50ml" },
       { item: "Ser Mozzarella Light", amount: "60g" },
-      { item: "Szynka drobiowa", amount: "3 plastry" },
-      { item: "Papryka czerwona", amount: "50g" }
+      { item: "Passata pomidorowa", amount: "50ml" },
+      { item: "Szynka drobiowa", amount: "3 plastry" }
     ],
-    recipe: "Zrób ciasto z mąki, wody i szczypty drożdży. Rozwałkuj cienko. Posmaruj passatą, posyp serem i ułóż dodatki. Piecz 10-12 min w 220 stopniach."
+    recipe: "Zrób ciasto z mąki i wody. Rozwałkuj cienko, nałóż passatę, ser i szynkę. Piecz 10 min w 220st."
   },
   {
     name: "Pulpety z indyka w sosie koperkowym",
@@ -101,10 +117,21 @@ export const MEAL_DATABASE: Meal[] = [
     type: "lunch",
     ingredients: [
       { item: "Pierś z indyka", amount: "150g" },
-      { item: "Koper świeży", amount: "1/2 pęczka" },
-      { item: "Ziemniaki", amount: "200g" }
+      { item: "Ziemniaki", amount: "200g" },
+      { item: "Koper świeży", amount: "1/2 pęczka" }
     ],
-    recipe: "Uformuj pulpety i gotuj w wywarze. Zrób sos z jogurtu i kopru. Podawaj z ziemniakami."
+    recipe: "Uformuj pulpety, gotuj w wywarze. Dodaj jogurt i koper do sosu. Podawaj z ziemniakami."
+  },
+  {
+    name: "Ryba po grecku w wersji lekkiej",
+    calories: 440, protein: 32, fats: 10, carbs: 48,
+    type: "lunch",
+    ingredients: [
+      { item: "Dorsz świeży", amount: "150g" },
+      { item: "Marchew", amount: "100g" },
+      { item: "Passata pomidorowa", amount: "100ml" }
+    ],
+    recipe: "Rybę upiecz. Warzywa zetrzyj i podduś z passatą. Przykryj rybę warzywami."
   },
   {
     name: "Gołąbki bez zawijania z indykiem",
@@ -118,18 +145,29 @@ export const MEAL_DATABASE: Meal[] = [
     recipe: "Wymieszaj mięso, ryż i posiekaną kapustę. Uformuj kotlety i piecz w sosie pomidorowym."
   },
   {
-    name: "Ryba po grecku w wersji fit",
-    calories: 440, protein: 32, fats: 10, carbs: 48,
+    name: "Gulasz z indyka z papryką",
+    calories: 510, protein: 35, fats: 14, carbs: 60,
     type: "lunch",
     ingredients: [
-      { item: "Dorsz świeży", amount: "150g" },
-      { item: "Marchew", amount: "100g" },
-      { item: "Passata pomidorowa", amount: "100ml" }
+      { item: "Pierś z indyka", amount: "150g" },
+      { item: "Papryka czerwona", amount: "100g" },
+      { item: "Kasza gryczana (sucha)", amount: "60g" }
     ],
-    recipe: "Rybę upiecz. Warzywa zetrzyj i podduś z passatą. Przykryj rybę warzywami."
+    recipe: "Indyka i paprykę podduś na łyżeczce oliwy. Podawaj z ugotowaną kaszą."
   },
   {
-    name: "Kurczak w sosie szpinakowym z makaronem",
+    name: "Zapiekanka z brokułem i kurczakiem",
+    calories: 530, protein: 38, fats: 15, carbs: 62,
+    type: "lunch",
+    ingredients: [
+      { item: "Pierś z kurczaka", amount: "120g" },
+      { item: "Makaron pełnoziarnisty", amount: "70g" },
+      { item: "Brokuły", amount: "150g" }
+    ],
+    recipe: "Makaron i brokuł ugotuj. Wymieszaj z kurczakiem i jogurtem. Zapiekaj 15 min."
+  },
+  {
+    name: "Kurczak w sosie szpinakowym",
     calories: 540, protein: 38, fats: 16, carbs: 62,
     type: "lunch",
     ingredients: [
@@ -138,6 +176,50 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Makaron pełnoziarnisty", amount: "70g" }
     ],
     recipe: "Kurczaka podsmaż, dodaj szpinak i jogurt. Wymieszaj z makaronem."
+  },
+  {
+    name: "Spaghetti Bolognese z indykiem",
+    calories: 540, protein: 32, fats: 14, carbs: 65,
+    type: "lunch",
+    ingredients: [
+      { item: "Makaron spaghetti pełnoziarnisty", amount: "70g" },
+      { item: "Mięso mielone z indyka", amount: "120g" },
+      { item: "Passata pomidorowa", amount: "150ml" }
+    ],
+    recipe: "Mięso podsmaż z czosnkiem, dodaj passatę. Wymieszaj z makaronem."
+  },
+  {
+    name: "Kotlety mielone z indyka z purée",
+    calories: 515, protein: 34, fats: 15, carbs: 56,
+    type: "lunch",
+    ingredients: [
+      { item: "Pierś z indyka", amount: "120g" },
+      { item: "Ziemniaki", amount: "200g" },
+      { item: "Buraczki tarte", amount: "100g" }
+    ],
+    recipe: "Uformuj kotlety z mielonego indyka i upiecz. Podawaj z purée i buraczkami."
+  },
+  {
+    name: "Pieczona dorada z cytryną",
+    calories: 460, protein: 32, fats: 18, carbs: 42,
+    type: "lunch",
+    ingredients: [
+      { item: "Dorada (świeża)", amount: "150g" },
+      { item: "Ziemniaki", amount: "150g" },
+      { item: "Pomidorki koktajlowe", amount: "100g" }
+    ],
+    recipe: "Rybę upiecz z cytryną i ziołami. Podawaj z pieczonymi ziemniakami."
+  },
+  {
+    name: "Leczo z cukinii i indyka",
+    calories: 480, protein: 30, fats: 12, carbs: 55,
+    type: "lunch",
+    ingredients: [
+      { item: "Pierś z indyka", amount: "120g" },
+      { item: "Cukinia", amount: "200g" },
+      { item: "Papryka czerwona", amount: "100g" }
+    ],
+    recipe: "Warzywa i mięso pokrój w kostkę i duś w passacie pomidorowej."
   },
   {
     name: "Schab pieczony z rozmarynem",
@@ -182,7 +264,7 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Serek wiejski lekki", amount: "150g" },
       { item: "Rzodkiewka", amount: "3 szt" }
     ],
-    recipe: "Wymieszaj rzodkiewkę z serkiem."
+    recipe: "Wymieszaj rzodkiewkę z serkiem wiejskim."
   },
   {
     name: "Bananowe placuszki (2 składniki)",
@@ -192,7 +274,7 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Banan", amount: "1 szt" },
       { item: "Jajko (rozmiar L)", amount: "1 szt" }
     ],
-    recipe: "Rozgnieć banana, wymieszaj z jajkiem. Smaż małe placuszki na suchej patelni."
+    recipe: "Rozgnieć banana, wymieszaj z jajkiem. Smaż małe placuszki."
   },
   {
     name: "Marchewka w słupkach z hummusem",
@@ -202,10 +284,10 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Marchew", amount: "150g" },
       { item: "Hummus", amount: "3 łyżki" }
     ],
-    recipe: "Marchewkę pokrój w słupki, maczaj w hummusie."
+    recipe: "Marchewkę pokrój w słupki, podawaj z hummusem."
   },
   {
-    name: "Koktajl truskawkowy z płatkami owsianymi",
+    name: "Koktajl truskawkowy z owsem",
     calories: 220, protein: 10, fats: 4, carbs: 38,
     type: "snack2",
     ingredients: [
@@ -213,20 +295,20 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Jogurt naturalny", amount: "150g" },
       { item: "Płatki owsiane", amount: "2 łyżki" }
     ],
-    recipe: "Zblenduj wszystkie składniki."
+    recipe: "Zblenduj wszystkie składniki na gładki koktajl."
   },
   {
-    name: "Garść orzechów włoskich i jabłko",
+    name: "Garść orzechów i jabłko",
     calories: 250, protein: 4, fats: 18, carbs: 22,
     type: "snack2",
     ingredients: [
       { item: "Orzechy włoskie", amount: "30g" },
       { item: "Jabłko", amount: "1 szt" }
     ],
-    recipe: "Chrupiąca i zdrowa przekąska."
+    recipe: "Szybka przekąska: chrupiące jabłko i zdrowe orzechy."
   },
 
-  // --- KOLACJE (dinner) ---
+  // --- KOLACJE (dinner / extra) ---
   {
     name: "Sałatka z tuńczykiem i jajkiem",
     calories: 310, protein: 28, fats: 12, carbs: 18,
@@ -236,7 +318,7 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Jajko (rozmiar L)", amount: "1 szt" },
       { item: "Mix sałat", amount: "garść" }
     ],
-    recipe: "Wymieszaj składniki, dopraw pieprzem."
+    recipe: "Wymieszaj składniki, dopraw pieprzem i sokiem z cytryny."
   },
   {
     name: "Sałatka jarzynowa fit",
@@ -247,6 +329,6 @@ export const MEAL_DATABASE: Meal[] = [
       { item: "Ziemniaki", amount: "100g" },
       { item: "Jogurt naturalny", amount: "2 łyżki" }
     ],
-    recipe: "Warzywa pokrój w kostkę, wymieszaj z jogurtem i musztardą."
+    recipe: "Ugotowane warzywa pokrój, wymieszaj z jogurtem i musztardą."
   }
 ];
